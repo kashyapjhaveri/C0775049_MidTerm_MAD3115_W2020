70666b977a1e94d599d7a75da8d76145332cdc65
Kashyap Jhaveri <jhaverikashyap1998@gmail.com>
2020-03-05 20:20:09 -0500

    Initial Commit

---
+++ C0775049_MidTerm_MAD3115_W2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0775049_MidTerm_MAD3115_W2020
+//
+//  Created by Kashyap Jhaveri on 2020-03-05.
+//  Copyright © 2020 Kashyap Jhaveri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
